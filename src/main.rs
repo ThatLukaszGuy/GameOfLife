180861d5ff277d4833b984588f5eeb4ff372e55d
Lukasz Jakubowski <lukasz.jakubowski@onet.eu>
2025-08-29 00:33:52 +0200

    basic working version - barebones

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,18 @@ use rand::Rng;
 const WIDTH: usize = 640;
 const HEIGHT: usize = 640;
 const CELL_SIZE: usize = 20; // size of each cell in pixels
+const CELL_COUNT:usize = 32; // DIM / CELL_SIZE have this 32x32 grid (cellcount per row/col) 
+const NEIGHBORS: [(isize, isize); 8] = [ 
+    (-1, -1), (0, -1), (1, -1),
+    (-1,  0),         (1,  0),
+    (-1,  1), (0,  1), (1,  1),
+]; // easier to check live neighbors
+const PROB: f64 = 0.05;
 
 struct Game {
     grid: Vec<Vec<bool>>,
-    first_display: bool // only run on first intial state
+    first_display: bool, // only run on first intial state
+    generation: usize
 }
 
 impl Game {
@@ -15,32 +23,82 @@ impl Game {
     fn new() -> Game {
         
         let mut rng = rand::thread_rng();
-        let grid: Vec<Vec<bool>> = (0..32)
+        let grid: Vec<Vec<bool>> = (0..CELL_COUNT)
         .map(|_| {
             (0..32)
-                .map(|_| rng.gen_bool(0.10)) // 10% chance true/false as alive cells should be rarer initially
+                .map(|_| rng.gen_bool(PROB)) // 10% chance true/false as alive cells should be rarer initially
                 .collect()
         })
         .collect();
 
         Game {
             grid,
-            first_display: false
+            first_display: false,
+            generation: 0
         }
 
     }
-    // mainly called for testing drawing - just call reset to test how fast frame rate looks
+    // reset board to some fresh random state
     fn reset(&mut self) {
         let mut rng = rand::thread_rng();
-        self.grid = (0..32)
+        self.grid = (0..CELL_COUNT)
         .map(|_| {
-            (0..32)
-                .map(|_| rng.gen_bool(0.10)) // 10% chance true/false as alive cells should be rarer initially
+            (0..CELL_COUNT)
+                .map(|_| rng.gen_bool(PROB)) // 10% chance true/false as alive cells should be rarer initially
                 .collect()
         })
         .collect();
         self.first_display = false;
     }
+
+    fn compute_next_state(&mut self) {
+        // to ensure a "synchronous" update keep a snapshot of old state and from it sequentially while referincng it 
+        // compute next state in a seperate grid -> then set that grid as the new grid
+
+        let mut updated_grid:Vec<Vec<bool>> = vec![vec![false; CELL_COUNT]; CELL_COUNT];
+
+        // each entry in the new grid is constructed from viewing the {entire/partly} old grid 
+        // RULES: (taken from https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life)
+        // 1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+        // 2. Any live cell with two or three live neighbours lives on to the next generation.
+        // 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+        // 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+        
+        for x in 0..CELL_COUNT {
+            for y in 0..CELL_COUNT {
+                
+                let alive_count = self.live_neighbors(x as isize, y as isize);
+                    
+                if self.grid[x][y] {
+                    // rule 1.
+                    if alive_count < 2 { updated_grid[x][y] = true }
+                    // rule 2.
+                    if alive_count == 2 || alive_count == 3 { updated_grid[x][y] = true }
+                    // rule 3.
+                    if alive_count > 3 { updated_grid[x][y] = false }
+                }
+                // rule 4.
+                if !self.grid[x][y] && alive_count == 3 { updated_grid[x][y] = true }
+            }
+        }
+
+        self.grid = updated_grid;
+        self.generation += 1;
+    }
+
+    fn live_neighbors(&mut self,x:isize,y:isize) -> usize {
+        NEIGHBORS.iter().filter(|&(dx,dy)| {
+            let row: isize = x + dx;
+            let col: isize= y + dy;
+                   
+            if row >= 0 && row < CELL_COUNT as isize && col >= 0 && col < CELL_COUNT as isize {
+                self.grid[row as usize][col as usize]
+            } else {
+                false
+            }
+        }).count()
+    }
+
 }
 
 fn main() {
@@ -57,7 +115,7 @@ fn main() {
         panic!("{}", e);
     });
 
-    window.set_target_fps(10);
+    window.set_target_fps(5);
 
     // game setup - init and create some randomized initial state
     let mut game = Game::new();
@@ -84,22 +142,23 @@ fn main() {
 }
 
 fn draw_grid(buffer: &mut [u32]) {
-    // clear screen to black
+    // clear screen to light beige
     for i in buffer.iter_mut() {
-        *i = 0x000000; 
+        *i = 0xF4EBD3; 
     }
 
+    // line color is darker beige
     // draw vertical grid lines
     for x in (0..WIDTH).step_by(CELL_SIZE) {
         for y in 0..HEIGHT {
-            buffer[y * WIDTH + x] = 0xFFFFFF; 
+            buffer[y * WIDTH + x] = 0xDED3C4; 
         }
     }
     
     // draw horizontal grid lines
     for y in (0..HEIGHT).step_by(CELL_SIZE) {
         for x in 0..WIDTH {
-            buffer[y * WIDTH + x] = 0xFFFFFF; 
+            buffer[y * WIDTH + x] = 0xDED3C4; 
         }
     }
 }
@@ -120,10 +179,10 @@ fn draw_state(game:&mut Game,buffer: &mut [u32]) {
     // we just draw initial state
     if !game.first_display {
 
-        for x in 0..32 {
-            for y in 0..32 {
+        for x in 0..CELL_COUNT {
+            for y in 0..CELL_COUNT {
                 if game.grid[x][y] {
-                    draw_cell(buffer, x, y, 0xFFFFFF); // white cell
+                    draw_cell(buffer, x, y, 0x555879); 
                 }
             }
         };
@@ -135,11 +194,12 @@ fn draw_state(game:&mut Game,buffer: &mut [u32]) {
     } else {
         // to replace with actual state update function 
         //game.reset();
+        game.compute_next_state();
 
-        for x in 0..32 {
-            for y in 0..32 {
+        for x in 0..CELL_COUNT {
+            for y in 0..CELL_COUNT {
                 if game.grid[x][y] {
-                    draw_cell(buffer, x, y, 0xFFFFFF); // white cell
+                    draw_cell(buffer, x, y, 0x555879); 
                 }
             }
         };
@@ -147,3 +207,5 @@ fn draw_state(game:&mut Game,buffer: &mut [u32]) {
 }
 
 //        draw_cell(&mut buffer, 10, 10, 0xFFFFFF); // white cell
+// 0x555879 navy
+// 0x98A1BC lighter blue
